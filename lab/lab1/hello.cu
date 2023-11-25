4d36684f9134fac524005378631290a4990db0ba
spoukke <sacha.bernheim@hey.com>
2023-11-25 12:52:37 +0100

    feat?(lab1): implement helloCuda kernel

--- lab/lab1/hello.cu
+++ lab/lab1/hello.cu
@@ -4,18 +4,27 @@
 __global__ void cudaHello(){
   // Afficher le message Hello World ainsi que blockidx et threadidx depuis chaque thread
   // A FAIRE ...
+  printf("Hello World from block %d, thread %d\n", blockIdx.x, threadIdx.x);
+  printf("Total number of blocks: %d, Total number of threads/block: %d\n", numBlocks, blockSize);
 }
 
 int main() {
-  int numBlocks = 64;
-  int blockSize = 1;
+  int totalThreads = 64;
+  int numBlocks;
+  int blockSize;
+  
   // Experimenter avec de differents blockSize (nombre de threads par block) pour les puissances de 2
   // tout en gardant le nombre total de threads egale a 64
   // A FAIRE ...
-  cudaHello<<<numBlocks, blockSize>>>(); 
+  for (blockSize = 64; blockSize >= 1; blockSize /= 2) {
+    numBlocks = totalThreads / blockSize;
+    printf("\nLaunching kernel with %d blocks and %d threads per block\n", numBlocks, blockSize);
+    cudaHello<<<numBlocks, blockSize>>>(numBlocks, blockSize); 
 
-  cudaError_t cudaerr = cudaDeviceSynchronize();
-  if (cudaerr != cudaSuccess)
-    printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
-  return 0;
-  }
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    if (cudaerr != cudaSuccess)
+      printf("kernel launch failed with error \"%s\".\n", cudaGetErrorString(cudaerr));
+    }
+
+    return 0;
+}
